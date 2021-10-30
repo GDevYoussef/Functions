b61384b1af09bcdc87036659619077557fda2301
GDevYoussef <gdevyoussef@gmail.com>
2021-10-30 10:23:54 +0200

    Update optional

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Net.Sockets;
 
 namespace Functions
 {
     class Program
     {
-        static void Main(string[] args)
+        private static float Add(float rhs, float lhs)
         {
-            Console.WriteLine("Hello World!");
+            return rhs + lhs;
+        }
+        
+        private static float Product(float rhs, float lhs)
+        {
+            return rhs * lhs;
+        }
+
+        private static float? Divide(float rhs, float lhs)
+        {
+            if (lhs == 0)
+            {
+                return null;
+            }
+            return rhs / lhs;
+        }
+        
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Add function result: " + Add(3.0f, 3.0f));
+            Console.WriteLine("Product function result: " + Product(3.0f, 3.0f));
+            Console.WriteLine("Divide function result: " + Divide(3.0f, 0));
         }
     }
 }
